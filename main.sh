6075648c5a56545308e925fafc98bb6420a8e8fa
chenxia130 <66128778+chenxia130@users.noreply.github.com>
2020-07-23 00:16:42 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -245,7 +245,7 @@ function setvhms(){
 }
 function Update(){
 	cd $tmpfile
-	links="http://kangle.odata.cc"
+	links="https://github.com/chenxia130/centos-6-7-kangle/raw/master"
 	wget -q $links/config -O $files/config
 	wget -q $links/main.sh -O main.sh;
 	cp -f main.sh /usr/bin/kanglesh
